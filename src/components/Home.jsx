506109870ad62ca01bf4fba6958f7eb14cd47a9e
Vinay Malyala <srivinay1432@gmail.com>
2025-04-03 23:19:12 +0530

    final commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,35 +1,36 @@
-import ImageUpload from './ImageUpload'
-import ImagePreview from './ImagePreview'
-import { useState } from 'react'
-import {enhancedImageAPI} from "../utils/enhanceImageApi"
+import ImageUpload from "./ImageUpload";
+import ImagePreview from "./ImagePreview";
+import { useState } from "react";
+import { enhancedImageAPI } from "../utils/enhanceImageApi";
 
 const Home = () => {
-  const [uploadImage, setUploadImage] = useState(null);
-  const [enhancedImage, setEnhancedImage] = useState(null);
-  const [loading, setLoading] = useState(false);
+    const [uploadImage, setUploadImage] = useState(null);
+    const [enhancedImage, setEnhancedImage] = useState(null);
+    const [loading, setloading] = useState(false);
 
-  const uploadImageHandler = async (file) => {
-    setUploadImage(URL.createObjectURL(file));
-    setLoading(true); 
+    const UploadImageHandler = async (file) => {
+        setUploadImage(URL.createObjectURL(file));
+        setloading(true);
+        try {
+            const enhancedURL = await enhancedImageAPI(file);
+            setEnhancedImage(enhancedURL);
+            setloading(false);
+        } catch (error) {
+            console.log(error);
+            alert("Error while enhancing the image. Please try again later.");
+        }
+    };
 
-    try {
-      const enhancedURL = await enhancedImageAPI(file);
-      setEnhancedImage(enhancedURL);
-      setLoading(false);
-    } catch (error) {
-      console.log(error);
-      alert("Error while enhancing image. Please try again.");
-      
-    }
-    
-  };
+    return (
+        <>
+            <ImageUpload UploadImageHandler={UploadImageHandler} />
+            <ImagePreview
+                loading={loading}
+                uploaded={uploadImage}
+                enhanced={enhancedImage?.image}
+            />
+        </>
+    );
+};
 
-  return (
-    <>
-        <ImageUpload uploadImageHandler={uploadImageHandler} />
-        <ImagePreview loading={loading} uploaded={uploadImage} enhanced={enhancedImage?.image} />
-    </>
-  )
-}
-
-export default Home
+export default Home;
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Loading = () => {
-  return (
-    <div className='flex justify-center items-center h-full'>
-      <div className="animate-spin border-4 border-t-transparent w-[3.4vw] h-[3.4vw] rounded-full border-gray-400"></div>
-    </div>
-  )
-}
+    return (
+        <div className="flex justify-center items-center h-full">
+            <div className="animate-spin border-4 border-t-transparent w-10 h-10 scale-200 rounded-full border-red-300"></div>
+        </div>
+    );
+};
 
-export default Loading
+export default Loading;
